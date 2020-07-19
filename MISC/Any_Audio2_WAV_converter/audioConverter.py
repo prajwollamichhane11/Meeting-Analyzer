4dd450e12a07583fc9f5edc81e788c1ad0f99b01
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2020-07-19 23:19:56 +0545

    New Updates Before Deployment

--- MISC/Any_Audio2_WAV_converter/audioConverter.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-
-audio_location = input()
-audio_format = audio_location.split(".")[1]
-
-
-wav_audio = AudioSegment.from_file(audio_location, format=audio_format)
-
-wav_audio.export("audio1.wav", format="wav")
