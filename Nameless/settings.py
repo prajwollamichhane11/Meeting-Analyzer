c17e19727943119c059a6120f07c7766c5d50ee2
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2020-07-19 23:53:44 +0545

    Ready for Deployment

--- Nameless/settings.py
+++ Nameless/settings.py
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
